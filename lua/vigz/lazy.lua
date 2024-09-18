e9a58f208c50f00df2ccb7583dc290ad2b269cef
Vigz <gabrielgaleza@gmail.com>
2024-09-19 00:28:45 +0200

    new plugs

--- lua/vigz/lazy.lua
+++ lua/vigz/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("vigz.plugins")
+require("lazy").setup({ import = "vigz.plugins" }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
